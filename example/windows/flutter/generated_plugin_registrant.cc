06a1a01648c93c92128758f5c790b8aa252e1da7
Kamil Kmetyk <kamil.kmetyk@darkfox.pl>
2025-03-17 10:53:15 +0100

    1.0.6

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <horizontal_compass/horizontal_compass_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  HorizontalCompassPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("HorizontalCompassPluginCApi"));
 }
